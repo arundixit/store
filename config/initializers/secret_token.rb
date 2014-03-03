e99abbb661bad64a4f51ea573f3562161cf78580
arundixit <arun.dixit@haatopia.in>
2014-03-03 11:59:06 +0530

    first store creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '90e6250ba5ea032248d2f898274b2196f681ae3f633021c391348476478555c789d148d4b6b7e56ae21a7c3db5db982661c09e76ba8ebdf1c89f50e283abdf64'
